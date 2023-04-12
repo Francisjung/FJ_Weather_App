bf65b411da7b66e84043dacf5435c931a28fd97f
Francis Jung <francisjung17@gmail.com>
2023-04-12 05:02:07 -0500

    added js changes

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,20 @@
-var apiKey ='575a38952d6eade22750975bfb1c7f22';
-function fetchAPIData(city){
-    var apiURl ="api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+APIKey;
+//https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key} 
+function fetchAPIData(){
+    var apiKey ='575a38952d6eade22750975bfb1c7f22';
+    var lat = 43.038902;
+    var lon = -87.906471;
+    var apiURl ="https://api.openweathermap.org/data/2.5/forecast?q=";
 
     fetch(apiURl).then(function (response) {
         if(response.ok){
-            
+            console.log(response.json())
+            return response.json();
         }
     })
 }
+fetchAPIData();
+
+
+var searchParams =  document.location.search.split('&');
+
+console.log(searchParams);
